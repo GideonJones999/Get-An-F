c8b304460422eac14511f5d5165c4beba09f0975
Gideon Jones <gideon.w.jones@gmail.com>
2020-06-11 10:45:53 -0600

    Init Commit

---
+++ GetAnF/GetAnF/Source.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+
+
+	using namespace std;
+
+	int main() 
+	{
+		string e = "elf", m = "mice", b = "bank", i = "ice";
+
+		cout << i << " " << b << " " << m << " " << e << endl;
+
+		system("PAUSE");
+		return 0;
+	}
